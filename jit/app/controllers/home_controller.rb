7cb6af6c682d48569c8e418334cc38da1c0b8f15
Maria Altebarmakian <m.altebarmakian@gmail.com>
2015-01-26 20:11:38 -0500

    Added different questions views and routed home view correctly

--- jit/app/controllers/home_controller.rb
+++ jit/app/controllers/home_controller.rb
@@ -5,10 +5,11 @@ class HomeController < ApplicationController
 #where should devise's before filer go? to verify that user is signed in before making any actions... (see devise site)
 
 def index 
-  
- #   if user_signed_in?
-#      redirect_to :controller => 'questions', action => 'index'
- #   end
+	if current_user.admin?
+		redirect_to(assignments_path)
+	else
+		redirect_to(questions_path)
+	end
 end
 
 #...
