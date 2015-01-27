8b02a31b376b3893060605a75e4ab3398ada45f6
Anna Yatskar <anyatskar@gmail.com>
2015-01-26 21:44:28 -0500

    play

--- jit/app/controllers/home_controller.rb
+++ jit/app/controllers/home_controller.rb
@@ -2,12 +2,10 @@
 
 class HomeController < ApplicationController
 
-#where should devise's before filer go? to verify that user is signed in before making any actions... (see devise site)
-
 def index 
 	if current_user.admin?
 		redirect_to(assignments_path)
-	else
+	else #NOT SUFFICIENT...
 		redirect_to(questions_path)
 	end
 end
