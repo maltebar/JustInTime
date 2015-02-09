efc4cfe3f71fa57802dab2db9fee8b6bce7959b0
Anna Yatskar <anyatskar@gmail.com>
2015-02-09 17:06:06 -0500

    Seed file incomplete

--- jit/db/seeds.rb
+++ jit/db/seeds.rb
@@ -9,11 +9,9 @@
 
 # test data 
 
-# 
-
-Assignment.create(title: 'Assignment 1', description: 'Test.', writer_due: '2015-03-01', promoter_due: '2015-04-01')
-Assignment.create(title: 'Assignment 2', description: 'Testing.', writer_due: '2015-04-02', promoter_due: '2015-04-09')
-Assignment.create(title: 'Assignment 3', description: 'Still Testing.', writer_due: '2015-04-10', promoter_due: '2015-04-21')
+# assignments created by professor
+Assignment.create(title: 'Assignment 1', description: 'What section number of chapter 1 did you find most confusing?', writer_due: '2015-03-03', promoter_due: '2015-03-10')
+Assignment.create(title: 'Assignment 2', description: "?????????", writer_due: '2015-01-01', promoter_due: '2015-02-20')
 
 Group.create(name: "Group 1")
 Group.create(name: "Group 2")
@@ -21,17 +19,19 @@ Group.create(name: "Group 2")
 # users
 User.create(name: 'Maria Altebarmakian', email:'m.altebarmakian@gmail.com', password: '12345678', admin: 'true')
 User.create(name: 'Anna Yatskar', email:'anyatskar@gmail.com', password: '12345678', admin: 'true')
-User.create(name: 'Billy Bob', email: 'billybob@gmail.com', password: '12345678', admin: 'false')
-User.create(name: 'Jane Jean', email: 'janejean@gmail.com', password: '12345678', admin: 'false')
-User.create(name: 'Sally Sue', email: 'sallysue@gmail.com', password: '12345678', admin: 'false')
-User.create(name: 'Bonnie Parker', email: 'bonnieparker@gmail.com', password: '12345678', admin: 'false')
-User.create(name: 'Clyde Barrow', email: 'clydebarrow@gmail.com', password: '12345678', admin: 'false')
+User.create(name: 'Billy Bob', email: 'billybob@gmail.com', password: '12345678', admin: 'false') #id3
+User.create(name: 'Jane Jean', email: 'janejean@gmail.com', password: '12345678', admin: 'false') #id4
+User.create(name: 'Sally Sue', email: 'sallysue@gmail.com', password: '12345678', admin: 'false') #id5
+User.create(name: 'Lola Lou', email: 'lolalou@gmail.com', password: '12345678', admin: 'false') #id6
+User.create(name: 'Betty Bee', email: 'bettybee@gmail.com', password: '12345678', admin: 'false') #id7
+User.create(name: 'John Jo', email: 'johnjo@gmail.com', password: '12345678', admin: 'false') #id8
+
+
 
 # questions written by users
 Question.create(content: 'Q1', user_id: '3', assignment_id: '1', user_name: 'Billy Bob')
 Question.create(content: 'Q2', user_id: '4', assignment_id: '1', user_name: 'Jane Jean')
 Question.create(content: 'Q3', user_id: '5', assignment_id: '1', user_name: 'Sally Sue')
-Question.create(content: 'Q4', user_id: '6', assignment_id: '1', user_name: 'Bonnie Parker')
-Question.create(content: 'Q5', user_id: '7', assignment_id: '1', user_name: 'Clyde Barrow')
-
-
+Question.create(content: 'Q4', user_id: '6', assignment_id: '2', user_name: 'Lola Lou')
+Question.create(content: 'Q5', user_id: '7', assignment_id: '2', user_name: 'Betty Bee')
+Question.create(content: 'Q6', user_id: '8', assignment_id: '2', user_name: 'John Jo')
