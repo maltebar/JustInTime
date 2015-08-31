de9de2d63b679f5215e457e7b28b317a7db6909c
kenharsch <kharsch@brandeis.edu>
2015-08-31 13:54:07 -0400

    server ready

--- jit/app/controllers/questions_controller.rb
+++ jit/app/controllers/questions_controller.rb
@@ -18,7 +18,7 @@ class QuestionsController < ApplicationController
       @assignment = Assignment.find(Assignment.where(active: true))
 
       if !@user.group.writer?
-        if @assignment.promoter_due > Time.now && Time.now > @assignment.writer_due
+        if @assignment.promoter_due > Time.zone.now && Time.zone.now > @assignment.writer_due
           if @assignment.reveal_1?
             if Evaluation.exists?(user_id: @user.id, assignment_id: @assignment.id, description_flag: 1)
               @evaluation_1 = Evaluation.find(Evaluation.where(user_id: @user.id, assignment_id: @assignment.id, description_flag: 1))
@@ -47,7 +47,7 @@ class QuestionsController < ApplicationController
         if @assignment.questions.exists?(user_id: current_user.id, description_flag: "1")
           @question_1 = Question.find(@assignment.questions.where(user_id: current_user.id, description_flag: "1"))
         else
-          if @assignment.description != "" && @assignment.writer_due > Time.now 
+          if @assignment.description != "" && @assignment.writer_due > Time.zone.now 
             @question_1 = Question.create(user_id: current_user.id, assignment_id: @assignment.id, description_flag: "1")
           end
         end
@@ -55,7 +55,7 @@ class QuestionsController < ApplicationController
         if @assignment.questions.exists?(user_id: current_user.id, description_flag: "2")
           @question_2 = Question.find(@assignment.questions.where(user_id: current_user.id, description_flag: "2"))
         else
-          if @assignment.description_2 != "" && @assignment.writer_due > Time.now 
+          if @assignment.description_2 != "" && @assignment.writer_due > Time.zone.now 
             @question_2 = Question.create(user_id: current_user.id, assignment_id: @assignment.id, description_flag: "2")
           end
         end
@@ -63,7 +63,7 @@ class QuestionsController < ApplicationController
         if @assignment.questions.exists?(user_id: current_user.id, description_flag: "3")
           @question_3 = Question.find(@assignment.questions.where(user_id: current_user.id, description_flag: "3"))
         else
-          if @assignment.description_3 != "" && @assignment.writer_due > Time.now 
+          if @assignment.description_3 != "" && @assignment.writer_due > Time.zone.now 
             @question_3 = Question.create(user_id: current_user.id, assignment_id: @assignment.id, description_flag: "3")
           end
         end
